ae5ef3c773fbea5a40dc46813b109418cb32e5a5
unknown <fateh_djehinet@outlook.com>
2021-02-08 22:22:18 +0100

    initialized pro

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { summaryHeaderItem } from '../SharedComponents/data-summary-header/data-summary-header.component';
 
 @Component({
   selector: 'app-home-page',
@@ -8,27 +6,9 @@ import { summaryHeaderItem } from '../SharedComponents/data-summary-header/data-
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  // displayedCol: string[] = [];
-  // data: MatTableDataSource<any> = new MatTableDataSource();
-  itemsGros!:summaryHeaderItem[];
-  items!:summaryHeaderItem[];
-  huile!:summaryHeaderItem[];
-  //TODO Profile Interface
+
   constructor() { }
   ngOnInit(): void {
-    this.itemsGros = [
-      { title: 'react', data: '7TW-##', icon: '' },
-      { title: 'tuy', data: '10MK####', icon: '' },
-      { title: 'VKA', data: '00:00:00', icon: '' },
-      { title: 'turbo', data: '1157', icon: '' },
-    ];
-    this.items = [
-      { title: 'Position Gauche', data: '', icon: '' },
-      { title: 'Position Droite', data: '', icon: '' },
-    ];
-    this.huile = [
-      { title: 'Huile', data: '', icon: '' },
-    ];
   }
 
 }
